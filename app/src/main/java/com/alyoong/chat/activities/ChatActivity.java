8d7e54c9df605a3e78e5480b35047e98cac44e5b
Kevin Januari <128971428+kevinjanuary@users.noreply.github.com>
2025-01-25 03:50:26 +0700

    chap 7

---
+++ app/src/main/java/com/alyoong/chat/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.alyoong.chat.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.alyoong.chat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
