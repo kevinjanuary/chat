99b6d3d94a79738eb866ef97953301e6cfdbc6aa
Kevin Januari <128971428+kevinjanuary@users.noreply.github.com>
2025-01-25 02:26:19 +0700

    chap 3

--- app/src/main/java/com/alyoong/chat/activities/MainActivity.java
+++ app/src/main/java/com/alyoong/chat/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.alyoong.chat;
+package com.alyoong.chat.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.alyoong.chat.R;
 
 public class MainActivity extends AppCompatActivity {
 
